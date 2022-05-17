6665380d38b873a1cebdd83d94f3da88f7583538
GH <georgeheath.98840@gmail.com>
2022-05-17 13:11:16 +0100

    GH: pushing return string representation of digits as no factor properties met, the test and method functionality

--- src/main/java/com/sparta/gh/raindrops/Raindrops.java
+++ src/main/java/com/sparta/gh/raindrops/Raindrops.java
@@ -1,4 +1,10 @@
 package com.sparta.gh.raindrops;
 
 public class Raindrops {
+
+  public static String raindrops( int n ) {
+    String string = String.valueOf( n );
+
+    return string;
+  }
 }
--- src/test/java/com/sparta/gh/raindrops/RaindropsTest.java
+++ src/test/java/com/sparta/gh/raindrops/RaindropsTest.java
@@ -1,5 +1,21 @@
 package com.sparta.gh.raindrops;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 class RaindropsTest {
 
+  @ParameterizedTest
+  @ValueSource(ints = {1, 4, 8, 11})
+  void returnDigitsOfNumberNoFactorRelation(int test){
+    // Given
+    String expected = String.valueOf( test );
+    // When
+    String result = Raindrops.raindrops(test);
+    // Then
+    Assertions.assertEquals( expected, result );
+  }
+
+  
 }
